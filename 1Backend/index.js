ecfd389c51fdabd6ed26158db43b79caaeb71868
Aryan <aryan.tambe04@gmail.com>
2025-07-24 20:49:03 +0530

    more routes

--- 1Backend/index.js
+++ 1Backend/index.js
@@ -5,8 +5,15 @@ const app=express();
 const port=3000; //total no. of ports are 65535 in the Interent Protocol Suite
 
 app.get('/',(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello World");
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("aryan-twitter");
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1> Login please</h1>');
 })
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
-})
+}) 
