75efa73a640db229e1f17eb789c0ce454c9f2476
Aryan <aryan.tambe04@gmail.com>
2025-07-26 01:58:20 +0530

    timestamps

--- 3mongoose/models/sub_todo.models.js
+++ 3mongoose/models/sub_todo.models.js
@@ -3,8 +3,12 @@ import mongoose from "mongoose";
 const subTodoSchema= new mongoose.Schema({
     content:{
         type:String,
-        required:true
+        required:true,
     },
+    complete:{
+        type:Boolean,
+        default:false,
+    }
     
 },{timestamps:true})
 
