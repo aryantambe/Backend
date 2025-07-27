954341711f41291450611635cd5617fba58a5921
Aryan <aryan.tambe04@gmail.com>
2025-07-27 22:54:43 +0530

    data modelling

---
+++ StreamNova/src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true, //for efficient searching
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model("User", userSchema);
