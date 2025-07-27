93892be06054f8acb40e81b2c0821ade67067be5
Aryan <aryan.tambe04@gmail.com>
2025-07-27 15:35:12 +0530

    connected to mongodb atlas in a separate file to maintain clean code

--- StreamNova/src/index.js
+++ StreamNova/src/index.js
@@ -1,5 +1,32 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+dotenv.config({
+    path:'./.env'
+});
+   
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* THIS IS A WAY TO CONNECT DB TO OUR APP, USING IIFEs, ASYNC AWAIT DIRECLY INSIDE THE INDEX.JS FILE
 
