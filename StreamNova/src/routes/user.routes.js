e3574d9920eac7e53208be4af159e8035ed1597b
Aryan <aryan.tambe04@gmail.com>
2025-07-29 00:17:38 +0530

    register user logic pending

--- StreamNova/src/routes/user.routes.js
+++ StreamNova/src/routes/user.routes.js
@@ -3,7 +3,9 @@ import { registerUser } from '../controllers/user.controller.js';
 
 const router=Router();
 
-router.route("/register").post(registerUser)
+router.route("/register")
+      .post(registerUser)
+      
 
 
 
