2ef32fca27b9d0cfd71cc631beebcce2d1a6eb23
Aryan <aryan.tambe04@gmail.com>
2025-07-28 23:15:28 +0530

    importing routers and using them as middlewares to keep the code base clean

--- StreamNova/src/app.js
+++ StreamNova/src/app.js
@@ -13,4 +13,16 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/users",userRouter) //control goes to userRouter, this segregation keeps files clean 
+
+
+
+
+
+
 export { app }   
