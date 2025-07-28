9ef516b64351dda4e9b0a3a7e0b74f5e9f127839
Aryan <aryan.tambe04@gmail.com>
2025-07-28 23:58:00 +0530

    user controller for registration and routes

--- StreamNova/src/app.js
+++ StreamNova/src/app.js
@@ -18,8 +18,8 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 
 //routes declaration
-app.use("/users",userRouter) //control goes to userRouter, this segregation keeps files clean 
-
+app.use("/api/v1/users",userRouter) //control goes to userRouter, this segregation keeps files clean 
+// the end result of this route would be- http://localhost:8000/api/v1/users/register or users/login ---> check the user.routes.js file for more info
 
 
 
