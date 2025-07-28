ef1f4681f782d8013b201a8385cb74eff4b94248
Aryan <aryan.tambe04@gmail.com>
2025-07-29 00:05:42 +0530

    debugging, ready for api testing

--- StreamNova/src/index.js
+++ StreamNova/src/index.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
+import express from "express";
 import connectDB from './db/index.js';
 dotenv.config({
     path:'./.env'
 });
    
-
+const app=express();
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
