0a7a563298c954a978d112d2b3903ccd078772b4
Aryan <aryan.tambe04@gmail.com>
2025-08-01 20:19:36 +0530

    created a method returning both access and refresh tokens

--- StreamNova/src/controllers/user.controller.js
+++ StreamNova/src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from '../utils/ApiResponse.js'
 
+const gernerateAccessAndRefreshTokens=async(userId)=>{
+     try {
+          const user=await User.findById(userId)
+          const accessToken=user.generateAccessToken()
+          const refreshToken=user.generateRefreshToken()
+
+          user.refreshToken=refreshToken
+          await user.save({validateBeforeSave:false})
+
+          return {accessToken,refreshToken}
+     } catch (error) {
+          throw new ApiError(500, "Something went wrong while generating refresh and access token")
+     }
+}
+
 const registerUser=asyncHandler(async (req,res)=>{
      //get user details from frontend
      //validation-not empty
@@ -86,7 +101,48 @@ const registerUser=asyncHandler(async (req,res)=>{
      
 })
 
+const loginUser= asyncHandler(async(req,res)=>{
+    //req.body--> data
+    //username or email 
+    //find the user
+    //check password if user exists
+    //generate a access and refresh token
+    // send cookie
+    //send message saying logged in Successfully
+
+    const {username,email,password}=req.body //destructuring
+
+    if(!username || !email){
+     throw new ApiError(400,"Username or email is required")
+    }
+
+   const user=await User.findOne({
+     $or:[{username},{email}]
+    })
+    if(!user){
+     throw new ApiError(404,"User does not exists");
+    }
+
+    const isPasswordValid=await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+     throw new ApiError(401,"Invalid user credentials");
+    }
+
+   const {accessToken,refreshToken}= await gernerateAccessAndRefreshTokens(user._id)
+ 
 
 
 
-export {registerUser};
+
+
+})
+
+
+
+
+export {
+     registerUser,
+     loginUser,
+
+};
